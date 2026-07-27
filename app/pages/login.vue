7b54e2ca031d027d0200425c52b7fa51d75cca48
karrarix <barzankarar@gmail.com>
2026-07-27 21:55:06 +0300

    auth page is done

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -1,13 +1,31 @@
 <script setup lang="ts">
 import type { AuthFormField,FormSubmitEvent } from '@nuxt/ui'
+
 const authForm = useTemplateRef('authForm');
 const supabase = useSupabaseClient();
 
-
+let mode = ref("LOG IN") ;
 const fields = ref<AuthFormField[]>([
   { name: 'email', type: 'email', label: 'Email' },
   { name: 'password', type: 'password', label: 'Password' }
 ])
+
+
+async function signUp(payload: FormSubmitEvent<any>){
+    const email= payload.data.email as string;
+    const password= payload.data.password as string;
+    const {error} = await supabase.auth.signUp({email,password})
+    if(error){
+        console.error(error)
+    }
+    else{
+    console.log("signup done with :", email, password)
+    window.location.href = "/login";
+
+    }
+};
+
+
 
   async function login(payload: FormSubmitEvent<any>) {
   const email = payload.data.email;
@@ -27,13 +45,38 @@ const fields = ref<AuthFormField[]>([
 };
 
 
+async function authup(payload: FormSubmitEvent<any>) {
+
+  if (mode.value === "LOG IN")
+{
+  login(payload)
+}
+else {
+  signUp(payload)
+}
+  
+}
+
+ function toggle(){
+  if (mode.value === "LOG IN"){
+    mode.value = "SIGN UP";
+    console.log(mode);
+  }else{mode.value = "LOG IN"
+  console.log(mode)
+  
+  }
+}
+
 </script>
 
 <template>
   <div class="flex flex-col justify-center top-2/12 items-center h-screen">
-    <UAuthForm title="Log in" ref="authForm" :fields="fields"  class="max-w-md" @submit="login">
+    <UAuthForm title="WELLCOME" ref="authForm" :fields="fields"  class="max-w-md" @submit="authup">
       <template #description>
-          Don't have an account? <ULink to="signup" class="text-primary font-medium">Sign up</ULink>.
+        <p class="my-4"> are you new sign up if not log in and wellcome back </p>
+
+        <UButton :label="mode" @click="toggle()" class=""></UButton>
+          
         </template>
     </UAuthForm>
   </div>
